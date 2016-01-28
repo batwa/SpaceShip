225326e22f3e8f9cc58e03b830c14fb131541123
Stammen <stammen.49@tso-phonesd3.asc.ohio-state.edu>
2016-01-28 14:58:48 -0500

    added PlayerShip object

---
+++ SpaceShip/PlayerShip.swift
@@ -0,0 +1,22 @@
+//
+//  PlayerShip.swift
+//  SpaceShip
+//
+//  Created by Stammen, Ben on 1/28/16.
+//  Copyright © 2016 Buckeye Hackers. All rights reserved.
+//
+
+import SpriteKit
+
+class PlayerShip: SKSpriteNode {
+    
+    init(){
+        let texture = SKTexture(imageNamed: "Spaceship")
+        super.init(texture: texture, color: SKColor.clearColor(), size: texture.size())
+        self.name = "PlayerShip"
+    }
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
+}
