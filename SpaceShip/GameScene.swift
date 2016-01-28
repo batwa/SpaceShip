13a57b2fd28c21f536fca0b217ebabc1ba4d7b33
Stammen <stammen.49@tso-phonesd3.asc.ohio-state.edu>
2016-01-28 15:16:40 -0500

    added function for rotating Sprite

--- SpaceShip/GameScene.swift
+++ SpaceShip/GameScene.swift
@@ -18,25 +18,24 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-//       /* Called when a touch begins */
-//        
-//        for touch in touches {
-//            let location = touch.locationInNode(self)
-//            
-//            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-//            
-//            sprite.xScale = 0.5
-//            sprite.yScale = 0.5
-//            sprite.position = location
-//            
-//            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-//            
-//            sprite.runAction(SKAction.repeatActionForever(action))
-//            
-//            self.addChild(sprite)
-//        }
+
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
     }
    
+    func rotateNode(node: SKNode, pointToFace: CGPoint){
+        
+        let angle:CGFloat = atan2(pointToFace.y - node.position.y, pointToFace.x - node.position.x)
+        
+        if(node.zRotation < 0){
+            node.zRotation = node.zRotation + CGFloat(M_PI * 2);
+        }
+        
+        node.runAction(SKAction.rotateToAngle(angle, duration: 0))
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
